70d41c78e3472fd50877b75c39496495f428f5ef
Arthur Korol <artur.korol.2000.10.25@mail.ru>
2023-07-22 22:06:27 +0300

    without animation

--- src/1page/mainPage.js
+++ src/1page/mainPage.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./mainPage.css";
 import AlphaLogo from "../exported files/AlphaLogo.svg";
 import Iphone14Pro from "../exported files/iPhone14Pro.svg";
+import LightTheme from "../exported files/LightTheme.svg";
+import firstCircle from "../exported files/circle/1first.svg";
+import secondCircle from "../exported files/circle/2second.svg";
+import thirdCircle from "../exported files/circle/3third.svg";
+import fourthCircle from "../exported files/circle/4fourth.svg";
+import fifthCircle from "../exported files/circle/5fifth.svg";
 
 export const MainPage = () => {
   return (
@@ -19,9 +25,30 @@ export const MainPage = () => {
         {/* <img src={Iphone14Pro} alt="alt" /> */}
       </div>
       <div className="menu">
-        <div className="LightTheme"></div>
-        <div className="Navigation"></div>
-        <div className="Language"></div>
+        <div className="LightTheme">
+          <img src={LightTheme} alt="alt" />
+        </div>
+        <div className="Navigation">
+          <p>Main page</p>
+          <p>Features</p>
+          <p>Soon</p>
+          <p>Create Wallet</p>
+        </div>
+        <div className="Language">
+          <h1>EN</h1>
+        </div>
+      </div>
+      <div className="circle">
+        {/* <img className="firstCircle" src={firstCircle} alt="alt" />
+        <img className="secondCircle" src={secondCircle} alt="alt" />
+        <img className="thirdCircle" src={thirdCircle} alt="alt" />
+        <img className="fourthCircle" src={fourthCircle} alt="alt" />
+        <img className="fifthCircle" src={fifthCircle} alt="alt" /> */}
+        <div className="firstCircle" />
+        <div className="secondCircle" />
+        <div className="thirdCircle" />
+        <div className="fourthCircle" />
+        <div className="fifthCircle" />
       </div>
     </div>
   );
